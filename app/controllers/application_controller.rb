d2fd643eadbcaaabb1f28588e3c5870150371854
mmccabe05 <mmccabe@chicagobooth.edu>
2022-10-25 02:36:34 +0000

    set up the initial site structure, very basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  def main_page
+      render({ :template => "main_page.html.erb" })
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "application", :action => "main_page" })
 
 
 
