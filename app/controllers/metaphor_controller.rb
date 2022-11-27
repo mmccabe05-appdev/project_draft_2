f88bdf0235803ac0ee3aa2f967f53d74e837cd69
mmccabe05 <mmccabe@chicagobooth.edu>
2022-11-27 21:54:48 +0000

    messed around with metaphor controller, no significant progress

--- app/controllers/metaphor_controller.rb
+++ app/controllers/metaphor_controller.rb
@@ -12,14 +12,22 @@ class MetaphorController < ApplicationController
     @all_reasons_2 = Reason.where(:neighborhood_id_2=>@metaphor_neighborhood.id)
 
     # a neighborhood id can show up in one of two columns in a metaphor
+    # create a hash for each neighborhood that fills :orig_neighborhood :compared_neighborhood :upvotes :downvotes :net_votes
     # find any reason that includes a @metaphor_neighborhood.id in either column, and make into one big array or hash
     # 
     # remove any reasons that aren't in @target_city
     # take the remaining reasons and tally up total upvotes and downvotes
     #
+    list_of_neighborhoods_and_vote_totals = Hash.new
 
     @result_neighborhood = @all_reasons 
 
+    @target_neighborhood = 0
+    @all_reasons.each do |a_reason|
+      if Neighborhood.where(:id=>a_reason.neighborhood_id_2).at(0) == 3
+        @target_neighborhood = @target_neighborhood + 1
+      end
+    end
     render({ :template => "show_metaphor.html.erb" })
   end
 
