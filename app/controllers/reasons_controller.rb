fc85fe3204a54f8d73638d9536db86b3c37fc674
mmccabe05 <mmccabe@chicagobooth.edu>
2022-12-04 21:27:09 +0000

    improved a bunch of formatting with bootstrap

--- app/controllers/reasons_controller.rb
+++ app/controllers/reasons_controller.rb
@@ -22,8 +22,8 @@ class ReasonsController < ApplicationController
     the_reason.user_id = @current_user.id
     the_reason.description = params.fetch("query_description")
     the_reason.link = params.fetch("query_link")
-    # the_reason.upvotes = params.fetch("query_upvotes")
-    # the_reason.downvotes = params.fetch("query_downvotes")
+    the_reason.upvotes = 1
+    the_reason.downvotes = 0
     the_reason.neighborhood_id_1 = params.fetch("query_neighborhood_id_1")
     the_reason.neighborhood_id_2 = params.fetch("query_neighborhood_id_2")
 
